740e08da7576e484365ef2fd2642302a333a15ff
nyhunter77 <nyhunter77@yahoo.com>
2015-08-29 12:32:07 -0400

    placing info on page

--- app.js
+++ app.js
@@ -1,81 +1,75 @@
-window.onload = function() {
+$(document).ready(function() {
 
-	// var workbook;  // when i try to make this a declared global var, it fails
-	var target = document.getElementById("list");
-	var br = document.createElement("br");
+	function parseExcel() {
+		// var workbook;  // when i try to make this a declared global var, it fails
+		var target = document.getElementById("list");
+		var br = document.createElement("br");
 
-	/* set up XMLHttpRequest */
-	var url = "info.xlsx";
-	var oReq = new XMLHttpRequest();
-	oReq.open("GET", url, true);
-	oReq.responseType = "arraybuffer";
+		/* set up XMLHttpRequest */
+		var url = "info.xlsx";
+		var oReq = new XMLHttpRequest();
+		oReq.open("GET", url, true);
+		oReq.responseType = "arraybuffer";
 
-	oReq.onload = function(e) {
-	  var arraybuffer = oReq.response;
+		oReq.onload = function(e) {
+		  var arraybuffer = oReq.response;
 
-	  /* convert data to binary string */
-	  var data = new Uint8Array(arraybuffer);
-	  var arr = new Array();
-	  for(var i = 0; i != data.length; ++i) arr[i] = String.fromCharCode(data[i]);
-	  var bstr = arr.join("");
+		  /* convert data to binary string */
+		  var data = new Uint8Array(arraybuffer);
+		  var arr = new Array();
+		  for(var i = 0; i != data.length; ++i) arr[i] = String.fromCharCode(data[i]);
+		  var bstr = arr.join("");
 
-	  /* Call XLSX */
-	  workbook = XLSX.read(bstr, {type:"binary"});  // want this to be a global var but doesnt seem to work
+		  /* Call XLSX */
+		  workbook = XLSX.read(bstr, {type:"binary"});  // want this to be a global var but doesnt seem to work
 
-	  /* DO SOMETHING WITH workbook HERE */
-	  // console.log(workbook);
+		  /* DO SOMETHING WITH workbook HERE */
 
-	  function to_json(workbook) {
-	    var result = {};
-	    workbook.SheetNames.forEach(function(sheetName) {
-	        var roa = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName]);
-	        if(roa.length > 0){
-	            result[sheetName] = roa;
-	        }
-	    });
-	    return result;
-	  }
+		  function to_json(workbook) {
+		    var result = {};
+		    workbook.SheetNames.forEach(function(sheetName) {
+		        var roa = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName]);
+		        if(roa.length > 0){
+		            result[sheetName] = roa;
+		        }
+		    });
+		    return result;
+		  }
 
-	  console.log(to_json(workbook));
-	  outputToScreen(workbook);
+		  console.log(to_json(workbook));
 
-	  function outputToScreen(workbook) {
-	  	for (var i = 0; i < workbook.Strings.length; i++) {
-		 		var text = document.createTextNode(workbook.Strings[i].t);
-		 		target.appendChild(text);
-		 		target.appendChild(br); 
-		 	};
-	  };
+		  // So I want to split this into a function of it's own but it seems jquery and workbook can't be accessed outside this .onload function
+			(function output() {  
+				var short = workbook.Sheets.Sheet1;
+			  $("#name").append(short.B3.v);
+			  $("#class").append(short.B8.v);
+			  $("#height").append(short.B9.v);
+			  $("#weight").append(short.B10.v);
+			  $("#gym").append(short.B11.v);
+			  $("#bday").append(short.B15.v);
+			  $("#country").append(short.B6.v);
+			  $("#nickname").append(short.B17.v);
+			  $("#stance").append(short.B18.v);
+			  $("#reach").append(short.B19.v);
+			  $("#discipline").append(short.B20.v);
+			  $("#promotor").append(short.B23.v);
+			  $("#wins").append(short.B29.v);
+			  $("#losses").append(short.B30.v);
+			  $("#draws").append(short.B31.v);
+			  $("#tko").append(short.B32.v);
+			  $("#submission").append(short.B33.v);
+			})();
+		}
 
-// outputToScreen(result);
+		oReq.send();
+	}
 
-	  // var sheet_name_list = workbook.SheetNames;
-	 	// sheet_name_list.forEach(function(y) { /* iterate through sheets */
-	  //   	worksheet = workbook.Sheets[y];
-	  //   	for (z in worksheet) {
-		 //        /* all keys that do not begin with "!" correspond to cell addresses */
-		 //        if (z[0] === '!') continue;
-		 //    	// console.log(y + "!" + z + "=" + JSON.stringify(worksheet[z].v));
-		 //    	myArray.push(JSON.stringify(worksheet[z]));
-	  //    	}
-	 	// });
+	parseExcel();
 
-	 	// for (var j=0; j < myArray.length; j++) {
-	 	// 	myParsed = JSON.parse(myArray[j]);
-	 	// 	console.log(myParsed.v);
-	 	// 	var text = document.createTextNode(myParsed.v);
-	 	// 	target.appendChild(text);
-	 	// 	target.appendChild(br); 
-	 	// }
-
-	 	// var name = JSON.parse(myArray[3]);
-	 	// target.appendChild(name.v);
-	 	// var nationality = JSON.parse(myArray[7]);
-	 	// target.appendChild(nationality.v); 
-
-	  }
-
-	oReq.send();
+}); 
 
 
-}; 
+
+
+
+
